930bb2d55694fd3c27a7251a6d8f5e0ed8d44886
meep-morp <gracieaerogirl@gmail.com>
2020-07-13 14:27:55 -0500

    Initialized project

---
+++ src/main/java/com/lambdaschool/java_orders/JavaOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.java_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/java_orders/JavaOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.java_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
